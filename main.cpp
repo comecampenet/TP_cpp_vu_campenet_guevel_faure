976b371ad2a40a2f18c20b36c5f97256beab0cb0
timy <timy@gmail.com>
2023-05-03 14:38:50 +0200

    come

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//suce moi la bite côme
 
 class RGB
 {
